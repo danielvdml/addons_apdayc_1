21a1c50b59edecdd61f89e65285df9b9de256b64
Daniel Moreno <danielvdmlfiis@gmail.com>
2024-02-03 12:05:00 -0500

    [UPD] * Botón desde contacto que permite redirigir a la lista de propiedades del contacto - Método dentro de un modelo - Llamada a una ventana desde un método en python - Extendimos un boton en la sección de smart buttons en el partner
    * Dentro del boton mostrar la cantidad de inmuebles del propietario
    - Uso de campos calculados

---
+++ inmobiliario_owners/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name":"Propietarios de inmuebles",
+    "summary":"Módulo para gestionar los propietarios de inmuebles",
+    "data":[
+        "views/res_partner.xml"
+    ],
+    "depends":[
+        "base",
+        "inmobiliario"
+    ]
+}
